950c08a315bb3c452b4982fa8675ca09d8f11965
adrian-gabor <adgabor97@gmail.com>
2025-07-09 15:14:21 +0200

    Add hamburger menu and navigation

--- src/js/fetchFeaturedProducts.js
+++ src/js/fetchFeaturedProducts.js
@@ -113,3 +113,22 @@ function displayProductListing(products) {
         }
     });
 }
+
+
+const hamburgerBtn = document.getElementById('hamburger-btn');
+const sideMenu = document.getElementById('side-menu');
+const closeMenuBtn = document.getElementById('close-menu-btn');
+
+function openMenu() {
+    sideMenu.classList.add('open');
+    document.body.classList.add('menu-open');
+}
+function closeMenu() {
+    sideMenu.classList.remove('open');
+    document.body.classList.remove('menu-open');
+}
+hamburgerBtn.addEventListener('click', openMenu);
+closeMenuBtn.addEventListener('click', closeMenu);
+sideMenu.addEventListener('click', function (e) {
+    if (e.target.classList.contains('navigation__link')) closeMenu();
+});
